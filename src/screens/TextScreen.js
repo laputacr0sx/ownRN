98f8139e9e4deff4c9b6aeba56d3c18652476e61
veilicsVV <laputafelix@gmail.com>
2022-03-25 18:27:31 +0800

    D25032022T1827 Text Screen with JSX

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, Text, TextInput } from 'react-native';
-import { borderColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
 const TextScreen = () => {
+	const [password, setPassword] = useState('');
+
 	return (
 		<View>
-			<Text style={style.text}>Text Screen</Text>
-			<TextInput style={style.input} />
+			<Text style={style.title}>Text Screen</Text>
+			<Text>Enter Password bellow:</Text>
+			<TextInput
+				style={style.input}
+				autoCapitalize='none'
+				autoCorrect={false}
+				value={password}
+				onChangeText={(newValue) => {
+					setPassword(newValue);
+				}}
+			/>
+			{password.length < 5 ? (
+				<Text>password must be longer than 5 characters</Text>
+			) : null}
 		</View>
 	);
 };
 
 const style = StyleSheet.create({
-	text: {
+	title: {
 		width: '100%',
 		textAlign: 'center',
 		fontSize: 36,
 		fontWeight: 'bold',
 	},
-	input: { margin: 15, borderColor: 'black', borderWidth: 1 },
+	input: { margin: 15, borderWidth: 1, color: 'black' },
+	text: {
+		fontSize: 20,
+		margin: 20,
+		textAlign: 'left',
+	},
 });
 
 export default TextScreen;
